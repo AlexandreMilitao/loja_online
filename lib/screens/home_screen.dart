52baa9e7bc7db34d88fbcda72f1b7fff63efbfd1
AleLevi <ale.levi@hotmail.com>
2022-06-21 21:32:49 -0300

    home_screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../tabs/home_tab.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  final _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      physics: const NeverScrollableScrollPhysics(),
+      children: <Widget>[HomeTab()],
+    );
+  }
+}
