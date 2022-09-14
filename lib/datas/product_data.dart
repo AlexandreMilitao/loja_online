78df00f9625644c5bc3774cf9b865747ff5aa6f8
AleLevi <ale.levi@hotmail.com>
2022-09-14 19:24:10 -0300

    ajuste login e criacaro de card

--- lib/datas/product_data.dart
+++ lib/datas/product_data.dart
@@ -20,4 +20,12 @@ class ProductData {
     price = snapshot.get("price") + 0.0;
     sizes = snapshot.get("sizes");
   }
+
+  Map<String, dynamic> toResumeMap() {
+    return {
+      "title": title,
+      "description": description,
+      "price": price,
+    };
+  }
 }
