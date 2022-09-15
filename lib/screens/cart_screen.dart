5de0b1dbdba68cb2e1d6f355025dcbf3af2a0d2d
AleLevi <ale.levi@hotmail.com>
2022-09-15 18:01:28 -0300

    ajustes_cart_screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loja_online/models/cart_model.dart';
+import 'package:loja_online/models/user_model.dart';
+import 'package:loja_online/screens/login_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatelessWidget {
@@ -12,16 +14,70 @@ class CartScreen extends StatelessWidget {
         title: Text('Meu carrinho'),
         actions: <Widget>[
           Container(
-            padding: const EdgeInsets.only(right: 8.0),
+            padding: const EdgeInsets.only(right: 8.0, top: 18.0),
             child: ScopedModelDescendant<CartModel>(
               builder: (context, child, model) {
                 int p = model.products.length;
                 return Text(
-                    '${p == null ? 0 : p} ${p == 1 ? "item" : "itens"}');
+                  '${p == null ? 0 : p} ${p == 1 ? "item" : "itens"}',
+                  style: TextStyle(fontSize: 17.0),
+                );
               },
             ),
           )
         ],
+      ),
+      body: ScopedModelDescendant<CartModel>(
+        builder: (context, child, model) {
+          if (model.isLoading && UserModel.of(context).isLoggedIn()) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (!UserModel.of(context).isLoggedIn()) {
+            return Container(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.remove_shopping_cart,
+                    size: 80.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  Text(
+                    "Faça o login para adcionar produtos!",
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => LoginScreen()));
+                      },
+                      child: const Text(
+                        'Entra',
+                        style: TextStyle(fontSize: 18.0),
+                      ))
+                ],
+              ),
+            );
+          } else if (model.products == null || model.products.length == 0) {
+            return const Center(
+              child: Text(
+                'Nenhum produto no carrinho!',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            );
+          } else {
+            return Container();
+          }
+        },
       ),
     );
   }
